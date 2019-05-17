3ee2e13ca74b0bfad34e8e629e0a0e7624206b27
admin <952798234@qq.com>
2019-05-17 14:55:16 +0800

    人员监控更新操作01

---
+++ src/main/java/com/erp/production/ssm/controller/employee/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.erp.production.ssm.controller.employee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Turtle
+ * @title: EmployeeController
+ * @projectName production_ms
+ * @description: TODO
+ * @date 2019/5/17 14:47
+ */
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeeController {
+}
