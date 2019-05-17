d0fb541f7800e5cb927f5ddcfd97352e1d266169
JayeUP <40747954+JayeUP@users.noreply.github.com>
2019-05-17 17:55:33 +0800

    plan 模块

--- src/main/java/com/erp/production/ssm/controller/employee/EmployeeController.java
+++ src/main/java/com/erp/production/ssm/controller/employee/EmployeeController.java
@@ -37,14 +37,7 @@ public class EmployeeController {
     public String findEmployee() {
         return  "employee_list";
     }
-
-    @RequestMapping(value = "/get_date")
-    public List<Employee> getData(DepartmentExample example) {
-        return employeeService.selectByExample(example);
-    }
-
-
-
+    
 
 
 }
